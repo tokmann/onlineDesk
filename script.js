16462a6338dd6b794a5f86eb892f33ba78576ef3
rodion <rodotokma@gmail.com>
2024-11-18 23:20:52 +0300

    Completed canvas, pen and eraser icons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// curtain move
 const curtain = document.getElementById('curtain')
 
 const curtain_handler = () => {
@@ -5,54 +6,81 @@ const curtain_handler = () => {
 }
 
 
-
-
-const curtainSize = document.getElementsByClassName("canvas")[0]
-const config = {
-    'color': 'black',
-    'lineSize': 3
-}
-
+// canvas drawing
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
+canvas.style.cursor = "url('https://img.icons8.com/?size=40&id=ZG5L7GewHO9G&format=png&color=000000'), auto"
+const config = {
+    'currentTool': 'pen',
+    'color': 'black',
+    'lineSize': 5
+}
 
-canvas.setAttribute('width', curtainSize.offsetWidth)
-canvas.setAttribute('height', curtainSize.offsetHeight)
+const pen_tool = () => {
+    ctx.lineWidth = 5
+    ctx.strokeStyle = 'black'
+    ctx.fillStyle = 'black'
 
-ctx.lineWidth = config.lineSize
-ctx.lineJoin = 'round'
-ctx.lineCap = 'round'
-ctx.strokeStyle = config.color
-ctx.fillStyle = config.color
+    config.currentTool = 'pen'
+    canvas.style.cursor = "url('https://img.icons8.com/?size=40&id=ZG5L7GewHO9G&format=png&color=000000'), auto"
+}
+
+const eraser_tool = () => {
+    ctx.lineWidth = 17
+    ctx.strokeStyle = 'white'
+    ctx.fillStyle = 'white'
+
+    config.currentTool = 'eraser'
+    canvas.style.cursor = "url('https://img.icons8.com/?size=40&id=78855&format=png&color=000000'), auto"
+}
 
 
-var isRec = false,
-    newDraw = false,
-    posX = [],
-    posY = []
+
+const setConfig = () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    ctx.lineWidth = config.lineSize
+    ctx.lineJoin = 'round'
+    ctx.lineCap = 'round'
+    ctx.strokeStyle = config.color
+    ctx.fillStyle = config.color
+}
+
+setConfig()
+window.addEventListener('resize', setConfig)
+
+const state = {
+    isRec: false,
+    newDraw: false
+}
+
+
 
 canvas.addEventListener("mousedown", (e) => {
-    if (isRec) return
+    if (state.isRec) return
     clearCanvas()
     canvas.onmousemove = (e) => recordMousePos(e)
 })
 
 const clearCanvas = () => {
-    if (newDraw) {
+    if (state.newDraw) {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        newDraw = false
+        state.newDraw = false
     }
     ctx.beginPath()
 }
 
 const recordMousePos = (e) => {
-    posX.push(e.clientX)
-    posY.push(e.clientY)
     drawLine(e.clientX, e.clientY)
 }
 
 const drawLine = (x, y) => {
-    ctx.lineTo(x, y)
+    if (config.currentTool == 'pen') {
+        ctx.lineTo(x + 13, y - 62)
+    }
+    else {
+        ctx.lineTo(x + 10, y - 65)
+    }
     ctx.stroke()
 }
 
@@ -60,6 +88,6 @@ canvas.addEventListener("mouseup", () => stopDrawing())
 
 const stopDrawing = () => {
     canvas.onmousemove = null
-    posX.push(undefined)
-    posY.push(undefined)
 }
+
+
